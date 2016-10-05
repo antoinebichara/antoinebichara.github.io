90f5053ffbed8d204f8bc2cb5d56b9126ca8b0e1
antoinebichara <antoinebichara@gmail.com>
2016-10-04 21:03:24 -0700

    w

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
- $('#square').click(function(){
-$('#square').fadeOut('slow);});
-});
-$(document).ready(function() {
-    $('div').mouseenter(function() {
-        $('div').fadeTo('fast', 1);
-    });
-    $('div').mouseleave(function(){
-	$('div').fadeTo('fast',0.5);
+ $('div').click(function(){
+$('div').fadeOut('slow);
 });
 });
